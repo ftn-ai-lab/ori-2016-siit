90d16b918cdbfb9b51da1dc933093dead40dddbb
Novke01 <anovakovic01@gmail.com>
2016-03-21 11:39:02 +0100

    Removed duplicated directory.

--- vezbe/01-linreg/README.md
+++
@@ -1,3 +0,0 @@
-## Vežba 1 - Linearna regresija
-
-Otvoriti datoteku **ori-2016-siit-01-linreg.ipynb**.
--- vezbe/01-linreg/src/skin_cancer.py
+++
@@ -1,4 +0,0 @@
-from __future__ import print_function
-
-# TODO 3: implementirati primenu jednostavne linearne regresije
-# nad podacima iz datoteke "data/skincancer.csv".
